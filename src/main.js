5cd94062ae6b07c4def5b03ce9f1fb8de6d8e29d
Dannie Hansen <danniehansenweb@gmail.com>
2020-06-08 21:37:46 +0200

    Started work on new back side

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const { cardBadges, cardButtons, cardDetailBadges, cardBackSection } = require('./shared.js');
+
+window.TrelloPowerUp.initialize({
+    'card-badges': cardBadges,
+    'card-buttons': cardButtons,
+    'card-detail-badges': cardDetailBadges,
+    'card-back-section': cardBackSection
+});
