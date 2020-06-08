5762faead32f7c802afaa4d737c8a0b80f4262c7
Dannie Hansen <danniehansenweb@gmail.com>
2020-06-08 23:16:20 +0200

    Added white modifier for timer status

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-const { cardBadges, cardButtons, cardDetailBadges, cardBackSection } = require('./shared.js');
+const { cardBadges, cardButtons, cardBackSection } = require('./shared.js');
 
 window.TrelloPowerUp.initialize({
+    'card-badges': cardBadges,
     'card-buttons': cardButtons,
-    'card-detail-badges': cardDetailBadges,
     'card-back-section': cardBackSection
 });
