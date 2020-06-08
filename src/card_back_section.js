c8354c5ed601f7bff555815729706d3fa8492548
Dannie Hansen <danniehansenweb@gmail.com>
2020-06-08 22:39:00 +0200

    Started work on new back side

--- src/card_back_section.js
+++ src/card_back_section.js
@@ -1,3 +1,9 @@
 const { isRunning, getTotalSeconds } = require('./shared.js');
 
 console.log('back side');
+
+var t = window.TrelloPowerUp.iframe();
+
+t.render(function() {
+    t.sizeTo('body');
+});
