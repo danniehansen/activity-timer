57bcc852acae08326b4ad869d2f3c55bb44a283e
Dannie Hansen <danniehansenweb@gmail.com>
2020-06-08 21:50:08 +0200

    Started work on new back side

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app.bundle.[hash].js'
+    filename: 'app.bundle.[name].[hash].js'
   },
   plugins: [
     new CleanWebpackPlugin(),
