6fff20fe0d9f0c339e37b72195227d638172bdd0
Dannie Hansen <danniehansenweb@gmail.com>
2020-06-15 21:40:18 +0200

    Added missing class properties plugin

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,8 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env'],
+            plugins: ["@babel/plugin-proposal-class-properties"]
           }
         }
       },
