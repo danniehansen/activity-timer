805eef1498251ceea9cd3e80c8ebb4b432730433
Dannie Hansen <danniehansenweb@gmail.com>
2020-06-17 22:53:56 +0200

    More work on estimate

--- src/js/views/change_estimate.js
+++ src/js/views/change_estimate.js
@@ -3,6 +3,20 @@ require('../../sass/main.scss');
 const { getOwnEstimate, createEstimate } = require('../shared.js');
 const t = window.TrelloPowerUp.iframe();
 
+const saveEstimateBtn = document.querySelector('.btn-save-estimate');
+const input = document.querySelector('input');
+
+getOwnEstimate(t).then((currentEstimate) => {
+    if (currentEstimate > 0) {
+        input.value = currentEstimate / 3600;
+    }
+});
+
+saveEstimateBtn.addEventListener('click', async () => {
+    await createEstimate(t, parseFloat(input.value) * 3600);
+    await t.closePopup();
+});
+
 t.render(async function() {
     t.sizeTo('body');
 });
