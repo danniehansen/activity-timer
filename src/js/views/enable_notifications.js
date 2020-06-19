bc7eab8f246dd956a5fab17c650c7117b65b6eae
Dannie Hansen <danniehansenweb@gmail.com>
2020-06-19 23:49:56 +0200

    Started work on estimate notifications feature

--- src/js/views/enable_notifications.js
+++ src/js/views/enable_notifications.js
@@ -1,19 +1,5 @@
-const {
-    disableNotificationsFeature,
-    enableNotificationsFeature
-} = require('../shared.js');
-
-const t = window.TrelloPowerUp.iframe();
-
-Notification.requestPermission().then(async (choice) => {
-    if (choice) {
-        await enableNotificationsFeature(t);
-    } else {
-        await disableNotificationsFeature(t);
-    }
-
+Notification.requestPermission().then(() => {
     window.close();
-}).catch(async () => {
-    await disableNotificationsFeature(t);
+}).catch(() => {
     window.close();
 });
--- src/js/views/notifications.js
+++ src/js/views/notifications.js
@@ -1,32 +1,20 @@
 require('../../sass/main.scss');
 
-const {
-    hasNotificationsFeature,
-    disableNotificationsFeature
-} = require('../shared.js');
-
 const t = window.TrelloPowerUp.iframe();
 const enableNotificationsBtn = document.querySelector('.btn-enable-notifications');
-const disableNotificationsBtn = document.querySelector('.btn-disable-notifications');
+const notificationsEnabled = document.querySelector('.notifications-enabled');
 
 enableNotificationsBtn.addEventListener('click', () => {
-   window.open(t.signUrl('./enable_notifications.html'));
-});
-
-disableNotificationsBtn.addEventListener('click', async () => {
-    await disableNotificationsFeature(t);
-    await render();
+   window.open('./enable_notifications.html');
 });
 
 async function render () {
-    const enabledNotifications = await hasNotificationsFeature(t);
-
-    if (Notification.permission === 'granted' && enabledNotifications) {
+    if (Notification.permission === 'granted') {
         enableNotificationsBtn.style.display = 'none';
-        disableNotificationsBtn.style.display = 'block';
+        notificationsEnabled.style.display = 'block';
     } else {
-        enableNotificationsBtn.style.display = 'block';
-        disableNotificationsBtn.style.display = 'none';
+        enableNotificationsBtn.style.display = 'none';
+        notificationsEnabled.style.display = 'block';
     }
 
     t.sizeTo('.notifications-wrapper');
