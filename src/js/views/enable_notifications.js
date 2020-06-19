35664468a2aa7ce77d35476a35bb29a09ee618c7
Dannie Hansen <danniehansenweb@gmail.com>
2020-06-19 23:41:39 +0200

    Started work on estimate notifications feature

--- src/js/views/enable_notifications.js
+++ src/js/views/enable_notifications.js
@@ -1,1 +1,19 @@
-Notification.requestPermission();
+const {
+    disableNotificationsFeature,
+    enableNotificationsFeature
+} = require('../shared.js');
+
+const t = window.TrelloPowerUp.iframe();
+
+Notification.requestPermission().then(async (choice) => {
+    if (choice) {
+        await enableNotificationsFeature(t);
+    } else {
+        await disableNotificationsFeature(t);
+    }
+
+    window.close();
+}).catch(async () => {
+    await disableNotificationsFeature(t);
+    window.close();
+});
