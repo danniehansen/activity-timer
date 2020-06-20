70a424302121392b613f59f652c263f03fa08f9e
Dannie Hansen <danniehansenweb@gmail.com>
2020-06-20 16:50:22 +0200

    Started work on estimate notifications feature

--- src/js/views/notifications.js
+++ src/js/views/notifications.js
@@ -1,25 +1,47 @@
 require('../../sass/main.scss');
 
+const {
+    hasNotificationsFeature,
+    enableNotificationsFeature,
+    disableNotificationsFeature
+} = require('../shared.js');
+
 const t = window.TrelloPowerUp.iframe();
 const enableNotificationsBtn = document.querySelector('.btn-enable-notifications');
-const notificationsEnabled = document.querySelector('.notifications-enabled');
+const disableNotificationsBtn = document.querySelector('.btn-disable-notifications');
 
-enableNotificationsBtn.addEventListener('click', () => {
-   window.open('./enable_notifications.html');
+enableNotificationsBtn.addEventListener('click', async () => {
+    if (Notification.permission === 'granted') {
+        await enableNotificationsFeature(t);
+    } else {
+        window.open('./enable_notifications.html');
+    }
 });
 
 async function render () {
-    if (Notification.permission === 'granted') {
+    const hasNotificationsEnabled = await hasNotificationsFeature(t);
+
+    if (hasNotificationsEnabled) {
         enableNotificationsBtn.style.display = 'none';
-        notificationsEnabled.style.display = 'block';
+        disableNotificationsBtn.style.display = 'block';
     } else {
         enableNotificationsBtn.style.display = 'none';
-        notificationsEnabled.style.display = 'block';
+        disableNotificationsBtn.style.display = 'block';
     }
 
     t.sizeTo('.notifications-wrapper');
 }
 
-setTimeout(render, 1000);
+setTimeout(async () => {
+    if (Notification.permission === 'granted') {
+        const hasNotificationsEnabled = await hasNotificationsFeature(t);
+
+        if (!hasNotificationsEnabled) {
+            await enableNotificationsFeature(t);
+        }
+    }
+
+    await render();
+}, 1000);
 
 t.render(render);
