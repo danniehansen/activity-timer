aac525a054245ee63dca0164af0974728463df51
Dannie Hansen <danniehansenweb@gmail.com>
2023-02-01 20:22:15 +0100

    Attempt at fixing export

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { InfrastructureStack } from '../lib/infrastructure-stack';
 
 const app = new cdk.App();
